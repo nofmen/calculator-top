db2828f3697f68d1e37ee867de6ba2b2bd2a0e93
Nathan <nofmen@gmail.com>
2022-02-08 22:02:14 -0500

    fail

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ let operation = "";
 let inputField = 0;
 let firstOperator = 0;
 let secondOperator = 0;
+let answer = 0;
 
 function doOperation(number, operation) {}
 
@@ -20,20 +21,41 @@ numberBtn.forEach((button) =>
 	})
 );
 
-operationBtn.forEach((button) =>
-	button.addEventListener("click", () => {
-		operation = button.textContent;
+function opHandler(operator) {
+	console.log(operator + ' operator');
+  console.log(firstOperator + ' first1');
+  console.log(secondOperator + ' second1');
 
-		if (inputDisplay.textContent == 0) {
-			outputDisplay.textContent = `${inputField} ${operation} `;
-		} else if (outputDisplay) {
-			outputDisplay.append(`${inputDisplay.textContent} ${operation} `);
-			inputField = inputDisplay.textContent;
-			inputDisplay.textContent = 0;
-      firstOperator = inputDisplay.textContent;
-		} else {
-      secondOperator = inputDisplay.textContent;
-			console.log(`${firstOperator} ${operation} ${secondOperator}`);
-		}
-	})
+	if (firstOperator === 0) {
+		firstOperator = inputDisplay.textContent;
+	} else if (secondOperator === 0) {
+		secondOperator = inputDisplay.textContent;
+	} else {
+		answer = calculateResult(firstOperator, secondOperator, operator);
+	}
+  console.log(answer + ' answer');
+  console.log(firstOperator + ' first2');
+  console.log(secondOperator + ' second2');
+}
+
+function calculateResult(num1, num2, operator) {
+  switch(operator) {
+    case '+':
+      return num1 + num2;
+      break;
+    case '-':
+      return num1 - num2;
+      break;
+    case '*':
+      return num1 * num2;
+      break;
+    case '/':
+      return num1 / num2;
+      break;
+    default:
+  }
+}
+
+operationBtn.forEach((button) =>
+	button.addEventListener("click", opHandler.bind(this, button.textContent))
 );
